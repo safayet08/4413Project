f489825e54d7354935ce1b2ad5a3c5eb9c1529b8
Kumar Vaibhav Jha <jha.kumarv@gmail.com>
2022-12-16 02:40:37 -0500

    more fixes to server.js

--- backend/server.js
+++ backend/server.js
@@ -110,7 +110,7 @@ if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../frontend/build")));
 
   app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+    res.sendFile(join(__dirname, "client", "build", "index.html"));
   });
 }
 
